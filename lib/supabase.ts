4d9f76fa8be78cb4690ac4ef35071cc31d50f5aa
Khyler Lin <lanexs777@gmail.com>
2025-05-12 22:50:19 -0500

    Updated supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,3 @@
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 
-export const supabase = createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+export const supabase = createClientComponentClient();
